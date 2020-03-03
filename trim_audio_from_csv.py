f14238be123b1642c8b922b0418af95454e256ba
Josh <heyjoshkruse@gmail.com>
2020-03-02 20:04:47 -0700

    refactored file and removed uneccasery code for our purposes

--- trim_audio_from_csv.py
+++ trim_audio_from_csv.py
@@ -1,22 +1,24 @@
+# imports
 import scipy.io.wavfile
 import numpy
 
+# set input, output
 filename = "s2lam001_180707_2018-07-09_20-20.wav"
 outputName = "output.wav"
 
+# read audio file
 fs1, y1 = scipy.io.wavfile.read(filename)
-l1 = numpy.array([  [7.2,19.8] ])
-l1 = numpy.ceil(l1*fs1)#get integer indices into the wav file - careful of end of array reading with a check for greater than y1.shape
-newWavFileAsList = []
-for elem in l1:
-  startRead = elem[0]
-  endRead = elem[1]
-  if startRead >= y1.shape[0]:
-    startRead = y1.shape[0]-1
-  if endRead >= y1.shape[0]:
-    endRead = y1.shape[0]-1
-  newWavFileAsList.extend( y1[ int(startRead) : int(endRead) ] )
 
-newWavFile = numpy.array(newWavFileAsList)
+# get seconds to trim
+sec_to_trim = numpy.array([7.2,19.8])
+# get integer indices into the wav file - careful of end of array reading with a check for greater than y1.shape
+sec_to_trim = numpy.ceil( sec_to_trim *fs1 )
 
-scipy.io.wavfile.write(outputName, fs1, newWavFile)
+# set start and end, if either is larger than the total file size, set to end of audio file
+startRead = sec_to_trim[0]
+endRead = sec_to_trim[1]
+if startRead >= y1.shape[0] : startRead = y1.shape[0]-1
+if endRead >= y1.shape[0]   :   endRead = y1.shape[0]-1
+
+# write to output file
+scipy.io.wavfile.write(outputName, fs1, y1[ int(startRead) : int(endRead) ])
