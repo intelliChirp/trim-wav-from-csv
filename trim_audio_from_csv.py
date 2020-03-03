9587d5edd2cc506ec0ba99013955904a8bacb9fe
Josh <heyjoshkruse@gmail.com>
2020-03-02 19:57:27 -0700

    successfully trims an audio file and saves to new file

--- trim_audio_from_csv.py
+++ trim_audio_from_csv.py
@@ -1,7 +1,12 @@
 import scipy.io.wavfile
+import numpy
+
+filename = "s2lam001_180707_2018-07-09_20-20.wav"
+outputName = "output.wav"
+
 fs1, y1 = scipy.io.wavfile.read(filename)
-l1 = numpy.array([  [7.2,19.8], [35.3,67.23], [103,110 ] ])
-l1 = ceil(l1*fs1)#get integer indices into the wav file - careful of end of array reading with a check for greater than y1.shape
+l1 = numpy.array([  [7.2,19.8] ])
+l1 = numpy.ceil(l1*fs1)#get integer indices into the wav file - careful of end of array reading with a check for greater than y1.shape
 newWavFileAsList = []
 for elem in l1:
   startRead = elem[0]
@@ -10,8 +15,7 @@ for elem in l1:
     startRead = y1.shape[0]-1
   if endRead >= y1.shape[0]:
     endRead = y1.shape[0]-1
-  newWavFileAsList.extend(y1[startRead:endRead])
-
+  newWavFileAsList.extend( y1[ int(startRead) : int(endRead) ] )
 
 newWavFile = numpy.array(newWavFileAsList)
 
